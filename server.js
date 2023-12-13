a7fc40c8fae4f293f5bcaace6ab6b3d4bd16aec3
ajiteshaggarwal <ajiteshaggarwal027@gmail.com>
2023-12-13 21:55:40 +1100

    .

---
+++ server.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+let port = process.env.port || 3000;
+require('./dbConnection');
+let router = require('./routers/router');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/api/cat',router);
+
+
+http.listen(port, ()=>{
+    console.log('express server started');
+});
